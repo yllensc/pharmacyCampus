d5511e2e540dc107b5c4677f6b238e086154f905
Marsh1100 <margie.b.agu17@gmail>
2023-09-19 17:50:19 -0500

    Modification  relations in User to employee and patient

--- API/Services/IUserService.cs
+++ API/Services/IUserService.cs
@@ -5,7 +5,6 @@ public interface IUserService
     Task<string> RegisterAsync(RegisterDto model);
     Task<string> RegisterAdmiAsync(RegisterAdmiDto model);
     Task<string> RegisterEmployeeAsync(RegisterEmployeeDto model);
-    Task<string> RegisterPatientAsync (RegisterPatientDto model);
    // Task<string> RegisterPatientAsync (RegisterPatientDto model);
     Task<DataUserDto> GetTokenAsync(LoginDto model);
     Task<string> AddRoleAsync(AddRoleDto model);
