25867af96444c0f7ba875cafa47718c93817a343
Yllen Santamaría <yllensc18@gmail.com>
2023-09-20 10:32:32 -0500

    repositorios yllen

---
+++ Application/Repository/PurchaseRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+using Persistence;
+
+namespace Application.Repository;
+    public class PurchaseRepository: GenericRepository<Purchase>, IPurchase
+{
+    private readonly PharmacyDbContext _context;
+
+    public PurchaseRepository(PharmacyDbContext context) : base(context)
+    {
+        _context = context;
+
+    }
+}
---
+++ Domain/Interfaces/IProvider.cs
@@ -0,0 +1,6 @@
+using Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Domain.Interfaces;
+    public interface IProvider: IGenericRepository<Provider>
+{}
