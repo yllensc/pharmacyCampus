0c4789490d6d6a00ce41e38bcbfd89e51eb393cc
Marsh1100 <margie.b.agu17@gmail>
2023-09-19 21:29:15 -0500

    feat: :sparkles: Register Patient!

--- API/Services/IUserService.cs
+++ API/Services/IUserService.cs
@@ -15,8 +15,11 @@ public interface IUserService
 =======
     Task<string> RegisterPatientAsync (RegisterPatientDto model);
 >>>>>>> 71f46c7 (feat: :sparkles: Register Patient!)
+<<<<<<< HEAD
 =======
 >>>>>>> origin/main
+=======
+>>>>>>> 1f490af (feat: :sparkles: Register Patient!)
     Task<DataUserDto> GetTokenAsync(LoginDto model);
     Task<string> AddRoleAsync(AddRoleDto model);
     Task<DataUserDto> RefreshTokenAsync(string refreshToken);
