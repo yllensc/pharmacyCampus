5df9e3b55f0a429d91c67471db0de1f9c00ae34d
Marsh1100 <margie.b.agu17@gmail>
2023-09-19 21:39:57 -0500

    feat: :sparkles: Create interface, repository of sale
    Also add Sale to UnitOfWork

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -16,7 +16,6 @@ public interface IUnitOfWork
     IProvider Providers {get; }
     IPurchase Purchases {get; }
     IPurchasedMedicine PurchasedMedicines {get; }
-    IPatient Patients { get; }
     Task<int> SaveAsync();
 
 }
