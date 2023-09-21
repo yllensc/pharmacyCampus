dae483c22d65d8c27d7ab7c2856a4e92ad8582ed
Marsh1100 <margie.b.agu17@gmail>
2023-09-21 14:26:01 -0500

    Add Idennumber patient

--- Domain/Entities/Patient.cs
+++ Domain/Entities/Patient.cs
@@ -10,6 +10,7 @@ namespace Domain.Entities;
         public string Name { get; set; }
         public string Address   { get; set; }
         public string PhoneNumber { get; set; }
+        public string IdenNumber { get; set; }
         public ICollection<Sale> Sales { get; set;}
 
     }
