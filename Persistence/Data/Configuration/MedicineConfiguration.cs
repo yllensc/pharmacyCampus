6d9cfc8d7c32fa92d565d0eff36ee948a9ffdc07
yllensc <yllensc18@gmail.com>
2023-09-22 15:10:44 -0500

    actualización DB medicine expireDate

--- Persistence/Data/Configuration/MedicineConfiguration.cs
+++ Persistence/Data/Configuration/MedicineConfiguration.cs
@@ -8,14 +8,22 @@ public class MedicineConfiguration : IEntityTypeConfiguration<Medicine>
 {
     public void Configure(EntityTypeBuilder<Medicine> builder)
     {
-        builder.ToTable("medicine");
+   builder.ToTable("medicine");
+
         builder.Property(p => p.Name)
         .IsRequired()
         .HasMaxLength(100);
+
+        builder.Property(p => p.Price)
+        .HasColumnType("decimal")
+        .IsRequired();
+
+        builder.Property(p => p.Stock)
+        .IsRequired()
+        .HasColumnType("int");
+        
         builder.HasOne(d => d.Provider)
         .WithMany(p => p.Medicines)
         .HasForeignKey(d => d.ProviderId);
-        builder.HasIndex(m => new { m.Name, m.ProviderId })
-        .IsUnique();
     }
 }
