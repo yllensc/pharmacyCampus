f629673da7f29f30810dda17e63514de1897c994
marsh1100 <margie.b.agu17@gmail.com>
2023-09-23 16:08:33 -0500

    feat: :recycle: Precio de la venta se calcule, correción code
    Evitar que se creen compras sin medicamentos asociados...

--- Application/Repository/SaleRepository.cs
+++ Application/Repository/SaleRepository.cs
@@ -21,65 +21,58 @@ public class SaleRepository : GenericRepository<Sale>, ISale
     }
     public async Task<string> RegisterAsync(Sale modelSale, SaleMedicine modelSaleMedicine)
     {
-        string dateSale= modelSale.DateSale.ToString("yyyy-MM-ddTHH:mm:ss.ffffffZ");
-        
-        if (DateTime.TryParseExact(dateSale, "yyyy-MM-ddTHH:mm:ss.ffffffZ", null, DateTimeStyles.None, out DateTime parseDate))
+        var newSale = new Sale
         {
-            var newSale = new Sale
-            {
-                DateSale = DateTime.UtcNow,
-                PatientId = modelSale.PatientId,
-                EmployeeId = modelSale.EmployeeId,
-                Prescription = modelSale.Prescription
-            };
+            DateSale = DateTime.UtcNow,
+            PatientId = modelSale.PatientId,
+            EmployeeId = modelSale.EmployeeId,
+            Prescription = modelSale.Prescription
+        };
 
-            try{
-                _context.Sales.Add(newSale);
-                await _context.SaveChangesAsync(); 
+        try{
+            _context.Sales.Add(newSale);
+            await _context.SaveChangesAsync(); 
 
-                var saleCreated = await _context.Sales
-                                        .Where(u=> u.Id == newSale.Id)
+            var saleCreated = await _context.Sales
+                                    .Where(u=> u.Id == newSale.Id)
+                                    .FirstOrDefaultAsync();
+            var medicine = await _context.Medicines
+                                        .Where(u=> u.Id == modelSaleMedicine.MedicineId)
                                         .FirstOrDefaultAsync();
-
-                var newSaleMedicine = new SaleMedicine
-                {
-                    SaleId = saleCreated.Id,
-                    MedicineId = modelSaleMedicine.MedicineId,
-                    SaleQuantity= modelSaleMedicine.SaleQuantity,
-                    Price = modelSaleMedicine.Price,
-                };
-                
-                try{
-                    var medicine = await _context.Medicines
-                                            .Where(u=> u.Id == modelSaleMedicine.MedicineId)
-                                            .FirstOrDefaultAsync();
-
-                    if (medicine.Stock >= newSaleMedicine.SaleQuantity)
-                    {
-                        medicine.Stock -= modelSaleMedicine.SaleQuantity;
-                    
-                        _context.SaleMedicines.Add(newSaleMedicine);
-                        _context.Medicines.Update(medicine);
-                        await _context.SaveChangesAsync();
-                    }else{
-                        _context.Sales.Remove(saleCreated);
-                        await _context.SaveChangesAsync();
-                        return "No hay tantos medicamentos";
-
-                    }
-                }catch(Exception ex){
-                    return ex.Message;
-                } 
-            }catch(Exception ex)
+            var newSaleMedicine = new SaleMedicine
             {
+                SaleId = saleCreated.Id,
+                MedicineId = modelSaleMedicine.MedicineId,
+                SaleQuantity= modelSaleMedicine.SaleQuantity,
+                Price = medicine.Price*modelSaleMedicine.SaleQuantity
+            };
+            try{
+                
+                if (medicine.Stock >= newSaleMedicine.SaleQuantity)
+                {
+                    medicine.Stock -= modelSaleMedicine.SaleQuantity;
+                
+                    _context.SaleMedicines.Add(newSaleMedicine);
+                    _context.Medicines.Update(medicine);
+                    await _context.SaveChangesAsync();
+                }else{
+                    _context.Sales.Remove(newSale);
+                    _context.Sales.Remove(saleCreated);
+                    await _context.SaveChangesAsync();
+                    return "No hay tantos medicamentos";
+
+                }
+            }catch(Exception ex){
                 return ex.Message;
-            }
-            
-            return "Sale made successfully!!";
-        }else
+            } 
+        }catch(Exception ex)
         {
-            return "Expiration Date hasn't correct format";
+            return ex.Message;
         }
+        
+        return "Sale made successfully!!";
+        
+        
        
     }
     public async Task<string> RegisterManyMedicinesAsync(Sale modelSale,  List<SaleMedicine> list)
@@ -104,18 +97,18 @@ public class SaleRepository : GenericRepository<Sale>, ISale
 
             foreach(var saleMedicine in list)
             {
+                var medicine = await _context.Medicines
+                                    .Where(u=>u.Id == saleMedicine.MedicineId)
+                                    .FirstOrDefaultAsync();
+                
                 
                 newSaleMedicines.Add(new SaleMedicine
                 {
                     SaleId = saleCreated.Id,
                     MedicineId = saleMedicine.MedicineId,
                     SaleQuantity= saleMedicine.SaleQuantity,
-                    Price = saleMedicine.Price,
+                    Price = medicine.Price*saleMedicine.SaleQuantity,
                 });
-
-                var medicine = await _context.Medicines
-                                    .Where(u=>u.Id == saleMedicine.MedicineId)
-                                    .FirstOrDefaultAsync();
 
                 if (medicine.Stock >= saleMedicine.SaleQuantity)
                 {
@@ -124,6 +117,7 @@ public class SaleRepository : GenericRepository<Sale>, ISale
                     _context.Medicines.Update(medicine);
                     await _context.SaveChangesAsync();
                 }else{
+                    _context.Sales.Remove(newSale);
                     _context.Sales.Remove(saleCreated);
                     await _context.SaveChangesAsync();
                     Console.WriteLine("medicamento creado");
